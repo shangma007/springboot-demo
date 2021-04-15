ed8b22ea398930c6b1f20be34cfa2f23abd8bec4
qjw <412684631@qq.com>
2021-04-15 19:49:04 +0800

    与idea集成首次提交

---
+++ src/main/java/com/sm/qy28/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.sm.qy28.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("demo")
+    public String demo(){
+        return "OK~~~~~~~~~~";
+    }
+}
